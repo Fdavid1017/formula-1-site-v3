45bd12a3bb091b3b65e03f6474e4fc1a823fcda2
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2021-06-10 13:27:47 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col>
+      Home
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
