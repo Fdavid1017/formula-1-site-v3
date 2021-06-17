f8cfe73492def2c51fafb6b7aefbda233efd5789
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2021-06-17 15:32:18 +0200

    extracted control arrows to its own component

---
+++ components/ControlArrows.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="control-buttons-container">
+    <v-row class="fill-width" align="center" justify="center">
+      <v-col>
+        <img class="left-button" src="~static/chevron.svg" @click="$emit('onLeftArrowClick')">
+      </v-col>
+      <v-col class="text-right">
+        <img class="right-button" src="~static/chevron.svg" @click="$emit('onRightArrowClick')">
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ControlArrows',
+  methods: {
+    onLeftArrowClick () {
+      this.$emit('onLeftArrowClick')
+    },
+    onRightArrowClick () {
+      this.$emit('onRightArrowClick')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "~assets/scss/drivers-background-animations.scss";
+
+.control-buttons-container {
+  position: absolute;
+  top: 10%;
+  width: 100%;
+  z-index: 3;
+
+  $chevron-hover-time: 0.25s;
+
+  .left-button,
+  .right-button {
+    width: 6em;
+    cursor: pointer;
+    transition: width $chevron-hover-time;
+
+    &:hover {
+      width: 5em;
+    }
+  }
+
+  $chevron-drop-shadow-x: 0.25em;
+  $chevron-drop-shadow-y: 0.25em;
+  $chevron-drop-shadow-blur: 0.4em;
+  $chevron-drop-shadow-color: rgba(0, 0, 0, 0.5);
+
+  @function neg($value) {
+    @return $value * -1
+  }
+
+  .left-button {
+    transform: rotateZ(180deg);
+    filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+    -webkit-filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+    -moz-filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+  }
+
+  .right-button {
+    filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+    -webkit-filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+    -moz-filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
+  }
+}
+</style>
--- pages/drivers.vue
+++ pages/drivers.vue
@@ -264,16 +264,7 @@
       </div>
     </v-container>
 
-    <div class="control-buttons-container">
-      <v-row class="fill-width" align="center" justify="center">
-        <v-col>
-          <img class="left-button" src="~static/chevron.svg" @click="onLeftArrowClick">
-        </v-col>
-        <v-col class="text-right">
-          <img class="right-button" src="~static/chevron.svg" @click="onRightArrowClick">
-        </v-col>
-      </v-row>
-    </div>
+    <control-arrows @onLeftArrowClick="onLeftArrowClick" @onRightArrowClick="onRightArrowClick" />
   </div>
   <div v-else>
     Please wait
@@ -281,8 +272,11 @@
 </template>
 
 <script>
+import ControlArrows from '@/components/ControlArrows'
+
 export default {
   name: 'Drivers',
+  components: { ControlArrows },
   data () {
     return {
       standings: null,
@@ -325,7 +319,7 @@ export default {
     }
   },
   mounted () {
-    this.$axios.$get('/api/get_drivers_standing/constructor').then((response) => {
+    this.$axios.$get('/api/driverStandings?order=constructor').then((response) => {
       this.standings = response
     })
   },
@@ -341,7 +335,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "~assets/scss/drivers-background";
+@import "~assets/scss/drivers-background-animations.scss";
 
 .container {
   width: 100%;
@@ -418,45 +412,4 @@ export default {
   }
 }
 
-.control-buttons-container {
-  position: absolute;
-  top: 10%;
-  width: 100%;
-  z-index: 3;
-
-  $chevron-hover-time: 0.25s;
-
-  .left-button,
-  .right-button {
-    width: 6em;
-    cursor: pointer;
-    transition: width $chevron-hover-time;
-
-    &:hover {
-      width: 5em;
-    }
-  }
-
-  $chevron-drop-shadow-x: 0.25em;
-  $chevron-drop-shadow-y: 0.25em;
-  $chevron-drop-shadow-blur: 0.4em;
-  $chevron-drop-shadow-color: rgba(0, 0, 0, 0.5);
-
-  @function neg($value) {
-    @return $value * -1
-  }
-
-  .left-button {
-    transform: rotateZ(180deg);
-    filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-    -webkit-filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-    -moz-filter: drop-shadow(neg($chevron-drop-shadow-x) neg($chevron-drop-shadow-x) $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-  }
-
-  .right-button {
-    filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-    -webkit-filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-    -moz-filter: drop-shadow($chevron-drop-shadow-x $chevron-drop-shadow-x $chevron-drop-shadow-blur $chevron-drop-shadow-color);
-  }
-}
 </style>
